32de3d771c3fd70fea7e1ef60c07c1a53ae224c0
Zack Schwartz <ztschwartz@icloud.com>
2021-06-20 12:44:43 -0500

    initial commit

---
+++ components/TaskItem.js
@@ -0,0 +1,12 @@
+import styles from '..styles/TaskItem.module.css'
+
+
+const TaskBox = () => {
+    return (
+        <div className={styles.container}>
+            This is interesting
+        </div>
+    )
+}
+
+export default TaskBox
